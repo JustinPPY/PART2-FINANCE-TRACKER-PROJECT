f9e57c5ba9a472036093053289816afeb74da1eb
JustinPPY <2005023C@student.tp.edu.sg>
2022-02-20 03:37:39 +0800

    Initial JUNIT installed.

---
+++ FinanceTrackerWebsite/src/test/java/LoginRegisterTest.java
@@ -0,0 +1,27 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LoginRegisterTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testDoGetHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDoPostHttpServletRequestHttpServletResponse() {
+		fail("Not yet implemented");
+	}
+
+}
